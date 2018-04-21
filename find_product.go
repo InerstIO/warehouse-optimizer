8c0ffba7cad74a6372db148547d167b8c53243c7
InerstIO <inerstio@gmail.com>
2018-04-20 19:40:53 -0700

    profiling for single item

--- find_product.go
+++ find_product.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
+	"runtime"
 	"strconv"
-	"strings"
+	//"strings"
 	"warehouse-optimizer/warehouse"
 )
 
@@ -14,24 +17,39 @@ const (
 	gridPath = "warehouse-grid.csv"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	m := warehouse.ParseProductInfo(gridPath)
 	pathInfo := warehouse.BuildPathInfo(gridPath)
-	fmt.Println("Hello User, where is your worker? e.g.:\"2 4\"")
-	x, y := warehouse.ReadInput()
+	//fmt.Println("Hello User, where is your worker? e.g.:\"2 4\"")
+	//x, y := warehouse.ReadInput()
+	x, y := 0, 0
 	start := warehouse.Point{X: x, Y: y}
 	if x*y%2 == 1 {
 		log.Fatal("Cannot start on a shelf.")
 	}
-	fmt.Println("What is your worker's end location? e.g.:\"0 18\"")
-	x, y = warehouse.ReadInput()
+	//fmt.Println("What is your worker's end location? e.g.:\"0 18\"")
+	//x, y = warehouse.ReadInput()
+	x, y = 0, 0
 	end := warehouse.Point{X: x, Y: y}
 	if x*y%2 == 1 {
 		log.Fatal("Cannot end on a shelf.")
 	}
 	var t int
 	for {
-		fmt.Println("Type 1 to manual input, type 2 to file input.")
+		/*fmt.Println("Type 1 to manual input, type 2 to file input.")
 		var strInput string
 		_, err := fmt.Scan(&strInput)
 		if err != nil {
@@ -41,13 +59,23 @@ func main() {
 		t, err = strconv.Atoi(strInput)
 		if err != nil {
 			log.Fatal(err)
-		}
+		}*/
+		t = 1
 		if t == 1 || t == 2 {
 			break
 		}
 	}
 	if t == 1 {
-		fmt.Println("Hello User, what items would you like to pick? (separate by space)")
+		prods := []int{46071, 379019, 70172, 1321, 2620261}
+		for i:=0;i<10000000;i++{
+			for _, prod := range prods {
+				dest := warehouse.FindDest(start, m[prod])
+				warehouse.FindPath(start, dest)
+				warehouse.FindPath(dest, end)
+			}
+		}
+		
+		/*fmt.Println("Hello User, what items would you like to pick? (separate by space)")
 		orders := warehouse.ReadOrder(m)
 		fmt.Println("Here is the optimal picking order:")
 		//optimalOrder := warehouse.BruteForceOrderOptimizer(orders[0], start, end, m, pathInfo)
@@ -56,7 +84,7 @@ func main() {
 		fmt.Println("Here is the optimal path:")
 		s := warehouse.Route2String(optimalOrder, start, end, m)
 		fmt.Println(s)
-		fmt.Printf("Total distance traveled: %v\n", warehouse.RouteLength(optimalOrder, start, end, m, pathInfo))
+		fmt.Printf("Total distance traveled: %v\n", warehouse.RouteLength(optimalOrder, start, end, m, pathInfo))*/
 	} else if t == 2 {
 		fmt.Println("Please list file of orders to be processed:")
 		ordersPath := warehouse.ReadString()
@@ -131,6 +159,18 @@ func main() {
 		}
 		fmt.Printf("%v orders processed.", orderCtr)
 	}
+
+	if *memprofile != "" {
+        f, err := os.Create(*memprofile)
+        if err != nil {
+            log.Fatal("could not create memory profile: ", err)
+        }
+        runtime.GC() // get up-to-date statistics
+        if err := pprof.WriteHeapProfile(f); err != nil {
+            log.Fatal("could not write memory profile: ", err)
+        }
+        f.Close()
+    }
 
 	/*prod, ok := m[id]
 	if ok {
