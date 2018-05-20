1f190ae251975696debd4656d0ea43d86b4d8056
InerstIO <inerstio@gmail.com>
2018-05-20 00:12:05 -0700

    sort orders by weight reverse

---
+++ warehouse/mergeOrders.go
@@ -0,0 +1,44 @@
+package warehouse
+
+import (
+	"sort"
+)
+
+// By is the type of a "less" function that defines the ordering of its Order arguments.
+type By func(o1, o2 *Order, m map[int]Product) bool
+
+// Sort is a method on the function type, By, that sorts the argument slice according to the function.
+func (by By) Sort(orders []Order, m map[int]Product) {
+	ps := &orderSorter{
+		orders: orders,
+		by:     by, // The Sort method's receiver is the function (closure) that defines the sort order.
+		m:      m,
+	}
+	sort.Sort(ps)
+}
+
+// orderSorter joins a By function and a slice of Orders to be sorted.
+type orderSorter struct {
+	orders []Order
+	by     func(o1, o2 *Order, m map[int]Product) bool // Closure used in the Less method.
+	m      map[int]Product
+}
+
+// Len is part of sort.Interface.
+func (s *orderSorter) Len() int {
+	return len(s.orders)
+}
+
+// Swap is part of sort.Interface.
+func (s *orderSorter) Swap(i, j int) {
+	s.orders[i], s.orders[j] = s.orders[j], s.orders[i]
+}
+
+// Less is part of sort.Interface. It is implemented by calling the "by" closure in the sorter.
+func (s *orderSorter) Less(i, j int) bool {
+	return s.by(&s.orders[i], &s.orders[j], s.m)
+}
+
+func ByWeightReverse(o1, o2 *Order, m map[int]Product) bool {
+	return OrderWeight(*o1, m) > OrderWeight(*o2, m)
+}
